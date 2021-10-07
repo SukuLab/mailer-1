7f704536f1830ed3d488ff184e6d7b98b91648a0
Gonzalo Torterolo <gtorterolo@zircon.tech>
2021-10-06 23:14:11 -0300

    upload build

--- dist/adapters/handlebars.adapter.js
+++ dist/adapters/handlebars.adapter.js
@@ -22,7 +22,7 @@ class HandlebarsAdapter {
         Object.assign(this.config, config);
     }
     compile(mailTemplate, mailContext, callback, mailerOptions) {
-        const precompile = (template, callback, options) => {
+        const precompile = (template, options) => {
             const templateExt = path.extname(template) || '.hbs';
             const templateName = path.basename(template, path.extname(template));
             const templateDir = template.startsWith('./')
@@ -31,16 +31,18 @@ class HandlebarsAdapter {
             const templateIdGetter = lodash_1.get(options, 'templateIdGetter', () => templateName);
             const templateId = templateIdGetter(template);
             const templatePath = path.join(templateDir, templateName + templateExt);
+            let error = null;
             if (!this.precompiledTemplates[templateId]) {
                 try {
                     const template = fs.readFileSync(templatePath, 'UTF-8');
                     this.precompiledTemplates[templateId] = handlebars.compile(template, lodash_1.get(options, 'options', {}));
                 }
                 catch (err) {
-                    return callback(err);
+                    error = err;
                 }
             }
             return {
+                error,
                 templateExt,
                 templateName,
                 templateId,
@@ -48,14 +50,17 @@ class HandlebarsAdapter {
                 templatePath,
             };
         };
-        const { templateId } = precompile(mailTemplate, callback, mailerOptions.template);
+        const { templateId, error } = precompile(mailTemplate, mailerOptions.template);
+        if (error) {
+            return callback(error);
+        }
         const runtimeOptions = lodash_1.get(mailerOptions, 'options', {
             partials: false,
             data: {},
         });
         if (runtimeOptions.partials) {
             const files = glob.sync(path.join(runtimeOptions.partials.dir, '*.hbs'));
-            files.forEach((file) => precompile(file, () => { }, runtimeOptions.partials));
+            files.forEach((file) => precompile(file, runtimeOptions.partials));
         }
         const rendered = this.precompiledTemplates[templateId](mailContext, Object.assign(Object.assign({}, runtimeOptions), { partials: this.precompiledTemplates }));
         if (this.config.inlineCssEnabled) {
